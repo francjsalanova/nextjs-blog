daa1986b181fbee42824f36158b9b7abb03f9c04
fsalanova <francjsalanova@gmail.com>
2021-11-06 11:40:54 +0100

    This is the title for the first commit.
    This is the body of the commit
    
    This can be as long as the user wants.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello' })
+    res.status(200).json({ text: 'Hello', change1: 'changed to test git', chage2: 'change to se diff git' })
   }
